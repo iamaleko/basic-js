ed8a3e429d75e43cbb6e57b44b634e71444f3445
Aleksandr Kolobov <aykolobov@avito.ru>
2025-11-09 00:15:58 +0800

    feat: solved encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  if (!str.length) {
+    return '';
+  }
+  let ans = ''
+  let count = 1;
+  let curr = str[0];
+  for (const char of str.slice(1)) {
+    if (char !== curr) {
+      ans += `${count > 1 ? count : ''}${curr}`;
+      count = 1;
+      curr = char;
+    } else {
+      count++;
+    }
+  }
+  return ans + `${count > 1 ? count : ''}${curr}`;
 }
 
 module.exports = {
